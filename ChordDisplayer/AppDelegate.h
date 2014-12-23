20c62720748d936b052ab6548bbbce82180be276
Johnny Parizek <JohnnyParizek@Pentwater-iMac-2012.local>
2014-12-22 19:22:56 -0500

    Initial Commit

---
+++ ChordDisplayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChordDisplayer
+//
+//  Created by Johnny Parizek on 12/22/14.
+//  Copyright (c) 2014 Johnny Parizek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChordDisplayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChordDisplayer
+//
+//  Created by Johnny Parizek on 12/22/14.
+//  Copyright (c) 2014 Johnny Parizek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
